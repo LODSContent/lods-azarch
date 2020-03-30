5371d3fc511c1ecf41560b509198ce7ab06a58dc
MGrasdal <martin_grasdal@hotmail.com>
2020-03-30 11:02:56 -0600

    Add files via upload

---
+++ ExampleWebApp/Controllers/ServiceTestController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ExampleWebApp.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace ExampleWebApp.Controllers
+{
+    [Route("service")]
+    public class ServiceTestController : Controller
+    {
+        private IConfiguration configuration;
+        public ServiceTestController(IConfiguration config)
+        {
+            configuration = config;
+        }
+        [HttpPost]
+        [Route("test")]
+        public ServiceCallResult Post( serviceSettings settings)
+        {
+            var result = new ServiceCallResult { Success = true };
+            try
+            {
+                if(string.IsNullOrEmpty(settings.Address))
+                {
+                    settings.Address = configuration["address"];
+                }
+                result.Data = ServiceCall.Call(settings);
+            } catch(Exception ex)
+            {
+                result.Success = false;
+                result.Data = ex.Message;
+            }
+            return result;
+        }
+    }
+}
---
+++ ExampleWebApp/wwwroot/js/site.js
@@ -0,0 +1,39 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+$(document).ready(function () {
+    $('#testAPI').click(testAPIClick);
+    function testAPIClick() {
+        var payload = {
+            'Address': $('#address').val(),
+            'Port': Number($('#port').val()),
+            'Command': Number($('.command:checked').val()),
+            'Minutes': 1
+        };
+        var test = JSON.stringify(payload);
+
+        $.post({
+            url: "/service/test",
+            data: payload,
+            dataType: "json"
+        }).done(function (data) {
+            var result = (data.success ? "Success: " : "Failure: ") + data.data;
+            $('#results').text(result);
+        }).fail(function () {
+            $('#results').text('Call failed');
+        });
+
+
+
+    }
+});
+
+function setCommand(command) {
+    if (command === '1') {
+        $("#minutesDiv").attr("hidden","hidden" );
+    } else {
+        $("#minutesDiv").removeAttr("hidden");
+
+    }
+}
